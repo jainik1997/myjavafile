cd5080353ba0399dcbc56b9e9c52c2a27e5dedea
jainikkumar patel <jainik1997@gmail.com>
2021-05-01 15:32:03 +0100

    jainik

---
+++ DataTypesArithmetic.java
@@ -0,0 +1,12 @@
+package homework;
+
+public class DataTypesArithmetic
+{public static void main (String []args){
+    byte n, a;
+
+    n=127;
+    a=-128;
+    System.out.println(n);
+    System.out.println(a);
+}
+}
